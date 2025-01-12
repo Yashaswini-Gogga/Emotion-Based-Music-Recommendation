8e5f552d4f9ac1d5d532141a60ff59147a4adfea
Yashaswini Gogga <yashaswini27g2.0@gmail.com>
2025-01-13 04:45:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,117 +1,101 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
+from streamlit_webrtc import webrtc_streamer
 import av
-import cv2
-import numpy as np
-import mediapipe as mp
+import cv2 
+import numpy as np 
+import mediapipe as mp 
 from keras.models import load_model
 import webbrowser
 
-# Load model and labels
-model = load_model("model.h5")
+model  = load_model("model.h5")
 label = np.load("labels.npy")
 holistic = mp.solutions.holistic
 hands = mp.solutions.hands
 holis = holistic.Holistic()
 drawing = mp.solutions.drawing_utils
 
-# Streamlit UI setup
 st.header("Emotion Based Music Recommender")
 
-# Initialize session state if not already set
 if "run" not in st.session_state:
-    st.session_state["run"] = "true"
+	st.session_state["run"] = "true"
 
-# Load emotion from file or set default
 try:
-    emotion = np.load("emotion.npy")[0]
+	emotion = np.load("emotion.npy")[0]
 except:
-    emotion = ""
+	emotion=""
 
-# Set the session state based on emotion
-if not emotion:
-    st.session_state["run"] = "true"
+if not(emotion):
+	st.session_state["run"] = "true"
 else:
-    st.session_state["run"] = "false"
+	st.session_state["run"] = "false"
 
-class EmotionProcessor(VideoProcessorBase):
-    def recv(self, frame):
-        frm = frame.to_ndarray(format="bgr24")
+class EmotionProcessor:
+	def recv(self, frame):
+		frm = frame.to_ndarray(format="bgr24")
 
-        ##############################
-        frm = cv2.flip(frm, 1)
+		##############################
+		frm = cv2.flip(frm, 1)
 
-        res = holis.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
+		res = holis.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
 
-        lst = []
+		lst = []
 
-        # Process face landmarks
-        if res.face_landmarks:
-            for i in res.face_landmarks.landmark:
-                lst.append(i.x - res.face_landmarks.landmark[1].x)
-                lst.append(i.y - res.face_landmarks.landmark[1].y)
+		if res.face_landmarks:
+			for i in res.face_landmarks.landmark:
+				lst.append(i.x - res.face_landmarks.landmark[1].x)
+				lst.append(i.y - res.face_landmarks.landmark[1].y)
 
-            # Process left hand landmarks
-            if res.left_hand_landmarks:
-                for i in res.left_hand_landmarks.landmark:
-                    lst.append(i.x - res.left_hand_landmarks.landmark[8].x)
-                    lst.append(i.y - res.left_hand_landmarks.landmark[8].y)
-            else:
-                for i in range(42):
-                    lst.append(0.0)
+			if res.left_hand_landmarks:
+				for i in res.left_hand_landmarks.landmark:
+					lst.append(i.x - res.left_hand_landmarks.landmark[8].x)
+					lst.append(i.y - res.left_hand_landmarks.landmark[8].y)
+			else:
+				for i in range(42):
+					lst.append(0.0)
 
-            # Process right hand landmarks
-            if res.right_hand_landmarks:
-                for i in res.right_hand_landmarks.landmark:
-                    lst.append(i.x - res.right_hand_landmarks.landmark[8].x)
-                    lst.append(i.y - res.right_hand_landmarks.landmark[8].y)
-            else:
-                for i in range(42):
-                    lst.append(0.0)
+			if res.right_hand_landmarks:
+				for i in res.right_hand_landmarks.landmark:
+					lst.append(i.x - res.right_hand_landmarks.landmark[8].x)
+					lst.append(i.y - res.right_hand_landmarks.landmark[8].y)
+			else:
+				for i in range(42):
+					lst.append(0.0)
 
-            # Reshape list for model prediction
-            lst = np.array(lst).reshape(1, -1)
+			lst = np.array(lst).reshape(1,-1)
 
-            # Make prediction
-            pred = label[np.argmax(model.predict(lst))]
-            print(pred)
-            cv2.putText(frm, pred, (50, 50), cv2.FONT_ITALIC, 1, (255, 0, 0), 2)
+			pred = label[np.argmax(model.predict(lst))]
 
-            # Save emotion prediction to file
-            np.save("emotion.npy", np.array([pred]))
+			print(pred)
+			cv2.putText(frm, pred, (50,50),cv2.FONT_ITALIC, 1, (255,0,0),2)
 
-        # Draw landmarks on the frame
-        drawing.draw_landmarks(frm, res.face_landmarks, holistic.FACEMESH_TESSELATION,
-                               landmark_drawing_spec=drawing.DrawingSpec(color=(0, 0, 255), thickness=-1, circle_radius=1),
-                               connection_drawing_spec=drawing.DrawingSpec(thickness=1))
-        drawing.draw_landmarks(frm, res.left_hand_landmarks, hands.HAND_CONNECTIONS)
-        drawing.draw_landmarks(frm, res.right_hand_landmarks, hands.HAND_CONNECTIONS)
+			np.save("emotion.npy", np.array([pred]))
 
-        ##############################
+			
+		drawing.draw_landmarks(frm, res.face_landmarks, holistic.FACEMESH_TESSELATION,
+								landmark_drawing_spec=drawing.DrawingSpec(color=(0,0,255), thickness=-1, circle_radius=1),
+								connection_drawing_spec=drawing.DrawingSpec(thickness=1))
+		drawing.draw_landmarks(frm, res.left_hand_landmarks, hands.HAND_CONNECTIONS)
+		drawing.draw_landmarks(frm, res.right_hand_landmarks, hands.HAND_CONNECTIONS)
 
-        return av.VideoFrame.from_ndarray(frm, format="bgr24")
 
-# Input fields for language and singer
+		##############################
+
+		return av.VideoFrame.from_ndarray(frm, format="bgr24")
+
 lang = st.text_input("Language")
-singer = st.text_input("Singer")
+singer = st.text_input("singer")
 
-# Conditionally run the WebRTC streamer
 if lang and singer and st.session_state["run"] != "false":
-    try:
-        webrtc_streamer(key=f"emotion_stream_{st.session_state['run']}", desired_playing_state=True,
-                        video_processor_factory=EmotionProcessor)
-    except Exception as e:
-        st.error(f"WebRTC streaming failed: {str(e)}")
+	webrtc_streamer(key="key", desired_playing_state=True,
+				video_processor_factory=EmotionProcessor)
 
-# Button to recommend songs
 btn = st.button("Recommend me songs")
 
 if btn:
-    if not emotion:
-        st.warning("Please let me capture your emotion first")
-        st.session_state["run"] = "true"
-    else:
-        # Open YouTube with search query
-        webbrowser.open(f"https://www.youtube.com/results?search_query={lang}+{emotion}+song+{singer}")
-        np.save("emotion.npy", np.array([""]))  # Clear emotion data after song recommendation
-        st.session_state["run"] = "false"
+	if not(emotion):
+		st.warning("Please let me capture your emotion first")
+		st.session_state["run"] = "true"
+	else:
+		webbrowser.open(f"https://www.youtube.com/results?search_query={lang}+{emotion}+song+{singer}")
+		np.save("emotion.npy", np.array([""]))
+		st.session_state["run"] = "false"
